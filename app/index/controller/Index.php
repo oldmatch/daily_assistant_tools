27d68a47434860299e9a3009949d767782e63ed9
lkz <250789360@qq.com>
2019-11-15 20:25:09 +0800

    没改完

--- app/index/controller/Index.php
+++ app/index/controller/Index.php
@@ -7,18 +7,6 @@ class Index
 {
     public function index()
     {
-        $citySelect = [
-            '东莞市',
-            '深圳市',
-            '广州市'
-        ];
-        $stationSelect = [
-            '东莞市' => ['理工学院体育中心（公交站）', '松山湖候机楼'],
-            '深圳市' => ['世界之窗地铁站C1出口'],
-            '广州市' => [],
-        ];
-        View::assign('citySelect', $citySelect);
-        View::assign('stationSelect', json_encode($stationSelect, 256));
         return view();
     }
 }
