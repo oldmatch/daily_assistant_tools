bcba25cf17c164a95c43fb5d41e9fd388ef51a0d
lkz <250789360@qq.com>
2019-11-15 15:46:10 +0800

    老王来了

--- app/index/controller/Bus.php
+++ app/index/controller/Bus.php
@@ -1,9 +1,11 @@
 <?php
 namespace app\index\controller;
 
+use app\BaseController;
+use think\facade\Request;
 use think\facade\View;
 
-class Bus
+class Bus extends BaseController
 {
     public function index()
     {
@@ -12,20 +14,21 @@ class Bus
 
     public function ask()
     {
+        $param = Request::instance()->param();
         try {
             $data = [
-                'cityStart' => '深圳市',
-                'stationStart' => '世界之窗地铁站C1出口',
-                'cityEnd' => '东莞市',
-                'stationEnd' => '理工学院体育中心（公交站）',
-                'schDate' => '2019-11-01',
+                'cityStart' => $param['cityStart'] ?? '深圳市',
+                'stationStart' => $param['stationStart'] ?? '世界之窗地铁站C1出口',
+                'cityEnd' => $param['cityEnd'] ?? '东莞市',
+                'stationEnd' => $param['stationEnd'] ?? '理工学院体育中心（公交站）',
+                'schDate' => $param['schDate'] ?? date('Y-m-d', time()),
                 'lineType' => '2',
                 'page' => '1',
-                'size' => '10',
-                'startIsHuanDao' => '2',
-                'endIsHuanDao' => '2',
-                'stationCode' => 'ST0194',
-                'dstStationCode' => 'ST0290',
+                'size' => '100',
+//                'startIsHuanDao' => '2',
+//                'endIsHuanDao' => '2',
+//                'stationCode' => 'ST0194',
+//                'dstStationCode' => 'ST0290',
             ];
             $header = [
                 'Content-Type: application/x-www-form-urlencoded'
@@ -33,7 +36,8 @@ class Bus
             $data = http_build_query($data);
             $res = curlRequest('https://passenger.wyebus.com/api/v2/passenger/plan/intercity/page', 'post', $data, $header);
             $res = json_decode($res, true);
-            $res = end($res['data']['pageEntity']['list']);
+//            $res = end($res['data']['pageEntity']['list']);
+            return json($res);
             if (!empty($res['surplus'])) {
                 $res = ['status' => 1, 'msg' => '有票了。。。。。。。' . $res['station']];
             } else {
