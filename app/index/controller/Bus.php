ff1ddc58ec94df0f44b3480f6bf6693d13177fef
lkz <250789360@qq.com>
2019-11-25 15:14:51 +0800

    完善样式

--- app/index/controller/Bus.php
+++ app/index/controller/Bus.php
@@ -15,11 +15,25 @@ class Bus extends BaseController
             '深圳市',
             '广州市'
         ];
+
         $stationSelect = [
-            '东莞市' => ['理工学院体育中心（公交站）', '松山湖候机楼'],
-            '深圳市' => ['世界之窗地铁站C1出口'],
-            '广州市' => [],
+//            '东莞市' => ['理工学院体育中心（公交站）', '松山湖候机楼'],
+//            '深圳市' => ['世界之窗地铁站C1出口'],
+//            '广州市' => [],
         ];
+
+        foreach ($citySelect as $value) {
+            // 根据接口获取站点
+            $data = curlRequest('https://passenger.wyebus.com/api/v3/city/station/list', 'post', http_build_query(['cityName' => $value, 'lineType' => 2, 'searchType' => 1]), ['Content-Type: application/x-www-form-urlencoded']);
+            $data = json_decode($data, true);
+            $all = $data['data']['districtList'][0]['stationList'];
+            $stationArr = [];
+            if (!empty($all)) {
+                $stationArr = array_column($all, 'stationName');
+            }
+            $stationSelect[$value] = $stationArr;
+        }
+
         View::assign('citySelect', $citySelect);
         View::assign('today', date('Y-m-d'));
         View::assign('stationSelect', json_encode($stationSelect, 256));
@@ -32,9 +46,9 @@ class Bus extends BaseController
         try {
             $data = [
                 'cityStart' => $param['cityStart'] ?? '深圳市',
-                'stationStart' => $param['stationStart'] ?? '世界之窗地铁站C1出口',
+                'stationStart' => $param['stationStart'] != '不限' ? $param['stationStart'] : '',
                 'cityEnd' => $param['cityEnd'] ?? '东莞市',
-                'stationEnd' => $param['stationEnd'] ?? '理工学院体育中心（公交站）',
+                'stationEnd' => $param['stationEnd'] !='不限' ? $param['stationEnd'] : '',
                 'schDate' => $param['schDate'] ?? date('Y-m-d', time()),
                 'lineType' => '2',
                 'page' => '1',
--- app/index/controller/Index.php
+++ app/index/controller/Index.php
@@ -1,7 +1,6 @@
 <?php
 namespace app\index\controller;
 
-use think\facade\View;
 
 class Index
 {
