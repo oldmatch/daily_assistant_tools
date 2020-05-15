5e4c03e1dcb90c89ffef7da41597a3da8e34a1cc
lkz <250789360@qq.com>
2020-05-15 18:05:20 +0800

    test海报

--- app/index/controller/Test.php
+++ app/index/controller/Test.php
@@ -68,84 +68,130 @@ class Test
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <title>双十一电商活动</title>
-  <style>
-    @font-face {
-        font-family: myFirstFont;
-         src: url('{$ttf}');
-    }
-    body {
-      font-family: myFirstFont;
-      width: 100%;
-      overflow-x: hidden;
-    }
-    * {
-      margin: 0;
-      padding: 0;
-      box-sizing: border-box;
-    }
-    
-    a {
-      display: block;
-    }
-    .bg1 {
-      width: 100%;
-      height: 0;
-      padding-bottom: 90%;
-      background: url('https://res2.owohpet.cn/web/owoh_11_electricity_activity/bg1.jpg') no-repeat center center;
-      background-size: cover;
-    }
-    .bg4 {
-      position: relative;
-      width: 100%;
-      height: 0;
-      padding-bottom: 124.13%;
-      background: url('https://res2.owohpet.cn/web/owoh_11_electricity_activity/bg4.jpg') no-repeat center center;
-      background-size: cover;
-    }
-    .qrcode-box {
-      position: absolute;
-      left: 50%;
-      bottom: 8%;
-      margin-left: -15%;
-      width: 30%;
-      height: 0;
-      padding-bottom: 30%;
-      box-shadow: 0.145rem 0.25rem 0.57rem rgba(116,64,116,0.5);
-    }
-    .text {
-      position: absolute;
-      bottom: 33%;
-      width: 100%;
-      text-align: center;
-      font-size: 0.24rem;
-      color: rgba(0, 0, 0, 0.5);
-    }
-    .qrcode {
-      position: absolute;
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 100%;
-    }
-  </style>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+    <title>双十一电商活动</title>
+    <script src="https://res2.owohpet.cn/web/rem.js"></script>
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+        body {
+            width: 100%;
+            overflow-x: hidden;
+        }
+        a {
+            display: block;
+        }
+        .bg1 {
+            width: 100%;
+            height: 0;
+            padding-bottom: 90%;
+            background: url('https://res2.owohpet.cn/web/owoh_11_electricity_activity/bg1.jpg') no-repeat center center;
+            background-size: cover;
+        }
+        .bg2 {
+            width: 100%;
+            height: 0;
+            padding-bottom: 117.73%;
+            background: url('https://res2.owohpet.cn/web/owoh_11_electricity_activity/bg2.jpg') no-repeat center center;
+            background-size: cover;
+        }
+        .bg3 {
+            width: 100%;
+            height: 0;
+            padding-bottom: 165.33%;
+            background: url('https://res2.owohpet.cn/web/owoh_11_electricity_activity/bg3.jpg') no-repeat center center;
+            background-size: cover;
+        }
+        .bg3_1 {
+            width: 100%;
+            height: 0;
+            padding-bottom: 41.73%;
+            background: url('https://res2.owohpet.cn/web/owoh_11_electricity_activity/bg3_1.jpg') no-repeat center center;
+            background-size: cover;
+        }
+        .bg3_2 {
+            width: 100%;
+            height: 0;
+            padding-bottom: 32.13%;
+            background: url('https://res2.owohpet.cn/web/owoh_11_electricity_activity/bg3_2.jpg') no-repeat center center;
+            background-size: cover;
+        }
+        .bg3_3 {
+            width: 100%;
+            height: 0;
+            padding-bottom: 31.33%;
+            background: url('https://res2.owohpet.cn/web/owoh_11_electricity_activity/bg3_3.jpg') no-repeat center center;
+            background-size: cover;
+        }
+        .bg3_4 {
+            width: 100%;
+            height: 0;
+            padding-bottom: 60.13%;
+            background: url('https://res2.owohpet.cn/web/owoh_11_electricity_activity/bg3_4.jpg') no-repeat center center;
+            background-size: cover;
+        }
+        .bg4 {
+            position: relative;
+            width: 100%;
+            height: 0;
+            padding-bottom: 124.13%;
+            background: url('https://res2.owohpet.cn/web/owoh_11_electricity_activity/bg4.jpg') no-repeat center center;
+            background-size: cover;
+        }
+        /*.qrcode-box {*/
+        /*    position: absolute;*/
+        /*    left: 50%;*/
+        /*    bottom: 8%;*/
+        /*    margin-left: -15%;*/
+        /*    width: 30%;*/
+        /*    height: 0;*/
+        /*    padding-bottom: 30%;*/
+        /*    box-shadow: 0.145rem 0.25rem 0.57rem rgba(116,64,116,0.5);*/
+        /*}*/
+        /*.text {*/
+        /*    position: absolute;*/
+        /*    bottom: 33%;*/
+        /*    width: 100%;*/
+        /*    text-align: center;*/
+        /*    font-size: 0.24rem;*/
+        /*    color: rgba(0, 0, 0, 0.5);*/
+        /*}*/
+        .qrcode {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+        }
+    </style>
 </head>
 <body>
-  <div class="container">
+<div class="container">
     <div class="bg1"></div>
+    <div class="bg2"></div>
+    <!-- <div class="bg3"></div> -->
+    <div class="bg3_1"></div>
+    <a class="bg3_2" href="owoh://eshop?region=china&url=https%3A%2F%2F100000578934.retail.n.weimob.com%2Fsaas%2Fretail%2F100000578934%2F240077934%2Fshop%2Findex%3Fessharewid%3D1430529241%26id%3D519228"></a>
+    <a class="bg3_3" href="owoh://eshop?region=china&url=https%3A%2F%2F100000578934.retail.n.weimob.com%2Fsaas%2Fretail%2F100000578934%2F240077934%2Fshop%2Findex%3Fessharewid%3D1430529241%26id%3D519228"></a>
+    <div class="bg3_4"></div>
     <div class="bg4">
-      <div class="text">
-        <p>OwOh 宠物生活馆官方客服</p>
-        <p>微信号: owoh0315</p>
-      </div>
-      <div class="qrcode-box">
-        <img class="qrcode" src="https://res2.owohpet.cn/web/owoh_11_electricity_activity/qrcode.png" alt="">
-      </div>
+        <div class="text">
+            <p>OwOh 宠物生活馆官方客服</p>
+            <p>微信号: owoh0315</p>
+        </div>
+        <div class="qrcode-box">
+            <img class="qrcode" src="https://res2.owohpet.cn/web/owoh_11_electricity_activity/qrcode.png" alt="">
+        </div>
     </div>
-  </div>
+</div>
 </body>
 </html>
+
 EOF;
         $output = __DIR__ . '/../../../public/demo/' . time() . '.jpg';
         $imageSnappy->generateFromHtml($htmlTemplate, $output);
