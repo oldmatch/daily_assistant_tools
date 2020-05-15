4b5ee86f56c4988211618a0585bf78197f8bbbe6
lkz <250789360@qq.com>
2020-05-15 17:14:46 +0800

    test海报

--- app/index/controller/Index.php
+++ app/index/controller/Index.php
@@ -1,59 +1,13 @@
 <?php
 namespace app\index\controller;
 
-use Knp\Snappy\Image;
 use think\facade\Db;
 
 class Index
 {
     public function index()
     {
-        require __DIR__ . '/../../../vendor/autoload.php';
-
-// 首先实例类，根据系统不同，选择不一样的脚本程序
-
-//        $imageSnappy = new \Knp\Snappy\Image(__DIR__ . '/../../../vendor/h4cc/wkhtmltoimage-amd64/bin/wkhtmltoimage-amd64');
-        $imageSnappy = new Image(__DIR__ . '/../../../vendor/h4cc/wkhtmltoimage-i386/bin/wkhtmltoimage-i386');
-
-// 如果有中文的话，需要添加中文字体
-//$ttf = __DIR__ . '/ttf/sc.ttf';
-// 需要合成的图片地址，base64也可以的，其实跟这个也无关，主要是html能静态展示，那么合成的就是什么样的
-        $image_url = 'https://www.php.net/images/logos/php-logo.svg';
-        $name = 'wkhtmltoimage';
-
-// html代码，随意搞了搞，真实使用的话，需要用心调整样式哦~
-        $htmlTemplate = <<<EOF
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>海报</title>
-</head>
-<style>
-    @font-face {
-        font-family: myFirstFont;
-    }
-
-    body {
-        font-family: myFirstFont;
-    }
-</style>
-<body>
-<img src="{$image_url}">
-<br>
-{$name}
-</body>
-</html>
-EOF;
-
-        $output = __DIR__ . '/../../../demo/' . time() . '.jpg';
-        $imageSnappy->generateFromHtml($htmlTemplate, $output);
-
-        echo 'ok';
-
-//        return view();
+        return view();
     }
 
     public function notPage()
---
+++ app/index/controller/Test.php
@@ -0,0 +1,73 @@
+<?php
+namespace app\index\controller;
+
+use Knp\Snappy\Image;
+
+class Test
+{
+    public function index()
+    {
+        require __DIR__ . '/../../../vendor/autoload.php';
+
+        // 首先实例类，根据系统不同，选择不一样的脚本程序
+        $imageSnappy = new Image(__DIR__ . '/../../../vendor/h4cc/wkhtmltoimage-amd64/bin/wkhtmltoimage-amd64');
+
+        // 如果有中文的话，需要添加中文字体
+        $ttf = __DIR__ . '/../../../public/ttf/sc.ttc';
+        // 需要合成的图片地址，base64也可以的，其实跟这个也无关，主要是html能静态展示，那么合成的就是什么样的
+        $image_url = 'https://www.php.net/images/logos/php-logo.svg';
+        $name = 'wkhtmltoimage';
+
+// html代码，随意搞了搞，真实使用的话，需要用心调整样式哦~
+        $htmlTemplate = <<<EOF
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>海报</title>
+</head>
+<style>
+    @font-face {
+        font-family: myFirstFont;
+        src: url('{$ttf}');
+    }
+
+    body {
+        font-family: myFirstFont;
+        text-align: center;
+        padding: 100px;
+        background: #8B91A0;
+    }
+
+    .image {
+        width: 300px;
+        height: 300px;
+        position: relative;
+    }
+
+    .content {
+        background: white;
+        width: 300px;
+        position: absolute;
+        top: 380px;
+        left: 520px;
+    }
+</style>
+<body>
+<img src="{$image_url}" class="image">
+<div class="content">
+    <text>{$name}</text>
+</div>
+</body>
+</html>
+EOF;
+
+        $output = __DIR__ . '/../../../public/demo/' . time() . '.jpg';
+        $imageSnappy->generateFromHtml($htmlTemplate, $output);
+
+        echo 'ok';
+    }
+}
